3eb7732d6885b403968a6a04531bc6460b2715df
Bernardo Ferrari <ber.97ferrari@gmail.com>
2024-09-19 15:19:53 -0300

    chore: adding type declaration for data

---
+++ src/app/types.ts
@@ -0,0 +1,13 @@
+export interface Drinks {
+    brand: string
+    crp: number
+    rank: number
+}
+
+export interface Fastfood {
+    brand: string
+    storeQuantity: number
+    startingDate: Date
+    upToDate: Date
+    infoSource: string
+}
