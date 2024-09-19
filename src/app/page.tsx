3a9bce9715b73ca913e796527924b7175c497b66
Bernardo Ferrari <ber.97ferrari@gmail.com>
2024-09-19 14:57:17 -0300

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+export default function HomePage() {
+  return ( 
+    <>
+      <section>
+        <h1>Bienvenidos a ARGENTO-CONSUMO</h1>
+        <p>La idea es comparar empresas argentinas con estadounidenses para ver cuanto nos falta para competirles en nuestro mercado local</p>
+      </section>
+    </>
+  )
+}
