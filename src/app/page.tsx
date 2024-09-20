ad44306de30c14a087ef42837ee8ddcf0a66b66b
Bernardo Ferrari <ber.97ferrari@gmail.com>
2024-09-20 09:19:56 -0300

    chore: extracting brands I want to filter

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,22 @@ import api from "./api/api"
 export default async function HomePage() {
   const drinks = await api.drink.list()
   const foods = await api.food.list()
+  let fizzyDrinks: any [] = [] 
+  const filterDrinksBrands = ['cunnington', 'manaos', 'coca-cola', 'pepsi']
+
+  drinks.forEach((drink) => {
+    let loweredBrand = drink.brand.toLocaleLowerCase()
+
+    filterDrinksBrands.forEach((brand) => {
+      if (loweredBrand == brand) {
+        fizzyDrinks.push(drink)
+      } else {
+        return
+      }
+    })
+
+    return fizzyDrinks
+  }) 
 
   return ( 
     <>
@@ -11,12 +27,21 @@ export default async function HomePage() {
         <p>La idea es comparar empresas argentinas con estadounidenses para ver cuanto nos falta para competirles en nuestro mercado local</p>
       
         <div>
-          {JSON.stringify(drinks, null, 2)}
+          <ul>
+            {
+              fizzyDrinks.map((drink) => (
+                <li className="flex flex-col">
+                  <span>{ drink.brand }</span>
+                  <span>{ drink.crp }</span>
+                </li>
+              ))
+            }
+          </ul>
         </div>
 
-        <div>
+        {/* <div>
           {JSON.stringify(foods, null, 2)}
-        </div>
+        </div> */}
       </section>
     </>
   )
